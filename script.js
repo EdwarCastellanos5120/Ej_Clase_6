09027b4e1adb38748cb82337005e0b83893d92e1
EdwarCastellanos5120 <ecastellanosp1@miumg.edu.gt>
2023-08-19 08:43:50 -0600

    VersionFinal

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function seleccionarArchivo() {
   return new Promise((resolve) => {
     setTimeout(() => {
       const archivoSeleccionado = archivo;
-      console.log('Archivo seleccionado: ' + archivoSeleccionado);
+      console.log("Archivo seleccionado: " + archivoSeleccionado);
       resolve(archivoSeleccionado);
     }, randomTime());
   });
@@ -15,8 +15,8 @@ function seleccionarArchivo() {
 function verificarFormato(nombreArchivo) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const formatoValido = nombreArchivo.endsWith('.txt');
-      console.log('Formato válido: ' + formatoValido);
+      const formatoValido = nombreArchivo.endsWith(".txt");
+      console.log("Formato válido: " + formatoValido);
       resolve(formatoValido);
     }, randomTime());
   });
@@ -27,7 +27,7 @@ function verificarTamaño(nombreArchivo, formatoValido) {
     setTimeout(() => {
       const tamañoAceptable = formatoValido && nombreArchivo.length < 20;
       resolve(tamañoAceptable);
-      console.log('Tamaño aceptable: ' + tamañoAceptable);
+      console.log("Tamaño aceptable: " + tamañoAceptable);
     }, randomTime());
   });
 }
@@ -38,7 +38,9 @@ function cargarArchivo(nombreArchivo, tamañoAceptable) {
       if (tamañoAceptable) {
         resolve(`El archivo ${nombreArchivo} se ha cargado exitosamente.`);
       } else {
-        reject(`El archivo ${nombreArchivo} no pudo ser cargado debido a su tamaño o formato.`);
+        reject(
+          `El archivo ${nombreArchivo} no pudo ser cargado debido a su tamaño o formato.`
+        );
       }
     }, randomTime());
   });
@@ -46,13 +48,37 @@ function cargarArchivo(nombreArchivo, tamañoAceptable) {
 
 const archivo = "archivo.txt";
 
-const botonSubir = document.getElementById('subir'); 
+const botonSubir = document.getElementById("subir");
+const textoContainer = document.getElementById("texto");
 
-botonSubir.addEventListener('click', () => {
+botonSubir.addEventListener("click", () => {
   seleccionarArchivo()
-    .then(nombreArchivo => verificarFormato(nombreArchivo))
-    .then(formatoValido => verificarTamaño(archivo, formatoValido))
-    .then(tamañoAceptable => cargarArchivo(archivo, tamañoAceptable))
-    .then(resultado => console.log(resultado))
-    .catch(error => console.error(error));
+    .then((nombreArchivo) => {
+      const span = document.createElement("span");
+      span.textContent = "Archivo seleccionado: " + nombreArchivo + "--> ";
+      textoContainer.appendChild(span);
+      return verificarFormato(nombreArchivo);
+    })
+    .then((formatoValido) => {
+      const span = document.createElement("span");
+      span.textContent = "Formato válido: " + formatoValido + "--> ";
+      textoContainer.appendChild(span);
+      return verificarTamaño(archivo, formatoValido);
+    })
+    .then((tamañoAceptable) => {
+      const span = document.createElement("span");
+      span.textContent = "Tamaño aceptable: " + tamañoAceptable + "--> ";
+      textoContainer.appendChild(span);
+      return cargarArchivo(archivo, tamañoAceptable);
+    })
+    .then((resultado) => {
+      const span = document.createElement("span");
+      span.textContent = resultado;
+      textoContainer.appendChild(span);
+    })
+    .catch((error) => {
+      const span = document.createElement("span");
+      span.textContent = "Error: " + error;
+      textoContainer.appendChild(span);
+    });
 });
